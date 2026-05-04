ca69cf0c8745933adb2015f7267ed15d05500bca
screwys <254296947+screwys@users.noreply.github.com>
2026-05-04 19:36:47 +0300

    stage efi files for iso

--- files/scripts/finalize.sh
+++ files/scripts/finalize.sh
@@ -6,5 +6,18 @@ s|^NAME=.*|NAME="nocblue"|
 s|^PRETTY_NAME=.*|PRETTY_NAME="nocblue Fedora 44 bootc"|
 EOF
 
+install_efi() {
+    local name="${1}"
+    local src
+
+    src="$(find /usr/lib/efi -path "*/EFI/fedora/${name}" -print -quit)"
+    test -n "${src}"
+    install -D -m 0644 "${src}" "/boot/efi/EFI/fedora/${name}"
+}
+
+install_efi shimx64.efi
+install_efi mmx64.efi
+install_efi gcdx64.efi
+
 find /etc/yum.repos.d -name '*.repo' -type f -print0 \
     | xargs -0 -r sed -i 's/^countme=1$/countme=0/'
