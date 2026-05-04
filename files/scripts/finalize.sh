af81398dd834168ff5bf22a0a020f908775a63ba
screwys <254296947+screwys@users.noreply.github.com>
2026-05-04 06:46:16 +0300

    added privacy defaults

--- files/scripts/finalize.sh
+++ files/scripts/finalize.sh
@@ -5,3 +5,6 @@ sed -i -f - /usr/lib/os-release <<'EOF'
 s|^NAME=.*|NAME="nocblue"|
 s|^PRETTY_NAME=.*|PRETTY_NAME="nocblue Fedora 44 bootc"|
 EOF
+
+find /etc/yum.repos.d -name '*.repo' -type f -print0 \
+    | xargs -0 -r sed -i 's/^countme=1$/countme=0/'
