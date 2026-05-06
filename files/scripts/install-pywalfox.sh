401b854d5d2d501c125d5b3b5429c71769a07d46
screwys <254296947+screwys@users.noreply.github.com>
2026-05-06 22:47:33 +0300

    fix user defaults and pywalfox install

--- files/scripts/install-pywalfox.sh
+++ files/scripts/install-pywalfox.sh
@@ -3,9 +3,16 @@ set -euo pipefail
 
 pywalfox_version="2.7.4"
 
+export PIPX_GLOBAL_HOME=/usr/lib/opt/pipx
+export PIPX_GLOBAL_BIN_DIR=/usr/bin
+export PIPX_GLOBAL_MAN_DIR=/usr/share/man
+
 pipx install --global --force --pip-args='--no-cache-dir' "pywalfox==${pywalfox_version}"
 
-/usr/local/bin/pywalfox install --global
+rm -f /usr/local/bin/pywalfox
 
-test -x /usr/local/bin/pywalfox
+/usr/bin/pywalfox install --global
+
+test -x /usr/bin/pywalfox
+test ! -e /usr/local/bin/pywalfox
 test -f /usr/lib/mozilla/native-messaging-hosts/pywalfox.json
