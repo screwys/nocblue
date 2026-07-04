d80faabb2cb09cbf674bcb5eff9a2af1dc661bd2
screwys <254296947+screwys@users.noreply.github.com>
2026-07-04 18:24:41 +0300

    fix(codex): remove nocblue sandbox wrapper

--- files/system/etc/fish/conf.d/nocblue-codex-bwrap.fish
+++
@@ -1,5 +0,0 @@
-if test -x /usr/bin/nocblue-codex
-    function codex --wraps codex --description 'Run Codex with the nocblue sandbox wrapper'
-        command /usr/bin/nocblue-codex $argv
-    end
-end
--- files/system/etc/profile.d/nocblue-codex-bwrap.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=sh
-if [ -x /usr/bin/nocblue-codex ]; then
-    codex() {
-        command /usr/bin/nocblue-codex "$@"
-    }
-fi
