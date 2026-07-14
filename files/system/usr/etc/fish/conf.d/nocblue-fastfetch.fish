bdd0cadf04fc1e3e07f451f1a174dd4050672eaf
screwys <254296947+screwys@users.noreply.github.com>
2026-07-14 12:50:16 +0300

    fix(fastfetch): preserve colors through renderer

--- files/system/usr/etc/fish/conf.d/nocblue-fastfetch.fish
+++ files/system/usr/etc/fish/conf.d/nocblue-fastfetch.fish
@@ -1,4 +1,4 @@
 function fastfetch --wraps fastfetch --description "Render fastfetch after it completes"
-    command fastfetch $argv | command cat
+    command fastfetch $argv --pipe false | command cat
     return $pipestatus[1]
 end
